5d3c2c826acde55ec86f140d2fc6b871a1edd016
Dan <507360715@qq.com>
2023-12-26 17:31:08 +0800

    Add long connect support for network.
    1. add feature.
    2. refactor code.

--- networks/p2pNetwork.go
+++ networks/p2pNetwork.go
@@ -1,8 +1,7 @@
-// something about broadcast
-
 package networks
 
 import (
+	"io"
 	"log"
 	"net"
 	"sync"
@@ -12,25 +11,44 @@ var connMaplock sync.Mutex
 var connectionPool = make(map[string]net.Conn, 0)
 
 func TcpDial(context []byte, addr string) {
-	var conn net.Conn
 	connMaplock.Lock()
 	defer connMaplock.Unlock()
-	if connectionPool[addr] == nil {
-		conn, err := net.Dial("tcp", addr)
+
+	var err error
+	var conn net.Conn // Define conn here
+	if c, ok := connectionPool[addr]; ok {
+		if tcpConn, tcpOk := c.(*net.TCPConn); tcpOk {
+			if err := tcpConn.SetKeepAlive(true); err != nil {
+				delete(connectionPool, addr) // Remove if not alive
+				conn, err = net.Dial("tcp", addr)
+				if err != nil {
+					log.Println("Reconnect error", err)
+					return
+				}
+				connectionPool[addr] = conn
+				go ReadFromConn(addr) // Start reading from new connection
+			} else {
+				conn = c // Use the existing connection
+			}
+		}
+	} else {
+		conn, err = net.Dial("tcp", addr)
 		if err != nil {
-			log.Println("connect error", err)
+			log.Println("Connect error", err)
 			return
 		}
 		connectionPool[addr] = conn
+		go ReadFromConn(addr) // Start reading from new connection
 	}
-	conn = connectionPool[addr]
 
-	_, err := conn.Write(append(context, '\n'))
+	_, err = conn.Write(append(context, '\n'))
 	if err != nil {
+		log.Println("Write error", err)
 		return
 	}
 }
 
+// Broadcast sends a message to multiple receivers, excluding the sender.
 func Broadcast(sender string, receivers []string, msg []byte) {
 	for _, ip := range receivers {
 		if ip == sender {
@@ -40,11 +58,29 @@ func Broadcast(sender string, receivers []string, msg []byte) {
 	}
 }
 
+// CloseAllConnInPool closes all connections in the connection pool.
 func CloseAllConnInPool() {
+	connMaplock.Lock()
+	defer connMaplock.Unlock()
+
 	for _, conn := range connectionPool {
 		conn.Close()
 	}
+	connectionPool = make(map[string]net.Conn) // Reset the pool
 }
 
-// todo
-// long connect (not close immediately) ...
+// ReadFromConn reads data from a connection.
+func ReadFromConn(addr string) {
+	conn := connectionPool[addr]
+	buffer := make([]byte, 1024)
+	for {
+		n, err := conn.Read(buffer)
+		if err != nil {
+			if err != io.EOF {
+				log.Println("Read error:", err)
+			}
+			break
+		}
+		log.Println("Received:", string(buffer[:n]))
+	}
+}
