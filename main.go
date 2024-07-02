75662b4c1f44c023c2ae04d24710e7f9b3834854
ye <yegiiiseeya@outlook.com>
2024-07-02 17:35:06 +0800

    rename

--- main.go
+++ main.go
@@ -7,13 +7,13 @@ import (
 )
 
 var (
-	shardNum int
-	nodeNum  int
-	shardID  int
-	nodeID   int
-	modID    int
-	isClient bool
-	isGen    bool
+	shardNum      int
+	nodeNum       int
+	shardID       int
+	nodeID        int
+	modID         int
+	isSuupervisor bool
+	isGen         bool
 )
 
 func main() {
@@ -21,8 +21,8 @@ func main() {
 	pflag.IntVarP(&nodeNum, "nodeNum", "N", 4, "indicate how many nodes of each shard are deployed")
 	pflag.IntVarP(&shardID, "shardID", "s", 0, "id of the shard to which this node belongs, for example, 0")
 	pflag.IntVarP(&nodeID, "nodeID", "n", 0, "id of this node, for example, 0")
-	pflag.IntVarP(&modID, "modID", "m", 3, "choice Committee Method,for example, 0, [CLPA_Broker,CLPA,Broker,Relay] ")
-	pflag.BoolVarP(&isClient, "client", "c", false, "whether this node is a client")
+	pflag.IntVarP(&modID, "modID", "m", 3, "choice Committee Method,for example, 0, [CLPA_Broker,CLPA,Broker,Relay]")
+	pflag.BoolVarP(&isSuupervisor, "supervisor", "c", false, "whether this node is a supervisor")
 	pflag.BoolVarP(&isGen, "gen", "g", false, "generation bat")
 	pflag.Parse()
 
@@ -32,7 +32,7 @@ func main() {
 		return
 	}
 
-	if isClient {
+	if isSuupervisor {
 		build.BuildSupervisor(uint64(nodeNum), uint64(shardNum), uint64(modID))
 	} else {
 		build.BuildNewPbftNode(uint64(nodeID), uint64(nodeNum), uint64(shardID), uint64(shardNum), uint64(modID))
